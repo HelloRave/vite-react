2707f0eb4a5fc0e8f319806748640048e0718739
weiwei.huang <weiwei.huang@brtchip.com>
2024-05-28 16:38:10 +0800

    Configure prettier and eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,8 +3,12 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -14,5 +18,24 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    indent: ['error', 2],
+    'eol-last': ['error', 'always'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-trailing-spaces': 'error',
+    'comma-style': ['error', 'last'],
+    'no-console': 0,
+    'func-call-spacing': ['error', 'never'],
   },
-}
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
