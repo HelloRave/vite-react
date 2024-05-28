a15ba7a4f300bed14ddb51d8b6fa4ea1c5262cde
weiwei.huang <weiwei.huang@brtchip.com>
2024-05-28 18:14:42 +0800

    Set up vitest

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,3 @@
 export default function App() {
-  return <h1 className='text-red-300'>Hello world</h1>;
+  return <h1 className="text-red-300">Hello world</h1>;
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./setupTests.ts'],
+    include: ['src/*.test.{ts,tsx}'],
+    exclude: ['node_modules'],
+    coverage: {
+      include: ['src/**/*.{ts,tsx}']
+    }
+  }
 });
